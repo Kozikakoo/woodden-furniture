af8820656d7511d5becd356183e98a019fb6ff63
Anastasia <kozikakoo@yandex.ru>
2023-10-11 12:27:26 +0300

    feat: footer column open

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const closeButtonBurger = document.querySelector('.header__close');
 const navMenu = document.querySelector('.header__nav-menu');
 const menuLink = document.querySelectorAll('.header__menu-link');
 
+const footerList = document.querySelectorAll('.footer__list');
+const footerColumn = document.querySelectorAll('.footer__column');
+const footerTitle = document.querySelectorAll('.footer__title');
+const footerTitleContacts = document.querySelector('.footer__title_additional');
+
 function openPopup(e) {
     popup.classList.add('popup_open');
     let parent = e.target.parentNode;
@@ -45,6 +50,13 @@ function toggleBurgerMenu() {
     navMenu.classList.toggle('active');
 }
 
+function openFooterList(e) {
+    console.log(e.currentTarget)
+    console.log(e.target)
+
+    footerList.classList.toggle('additional');
+}
+
 
 
 closeButton.addEventListener('click', closePopup);
@@ -55,3 +67,6 @@ popup.addEventListener('click', (e) => closePopupByOverlay(e));
 burgerButton.addEventListener('click', toggleBurgerMenu);
 closeButtonBurger.addEventListener('click', toggleBurgerMenu);
 menuLink.forEach((item) => item.addEventListener('click', toggleBurgerMenu));
+
+footerColumn.forEach((item) => item.addEventListener('click', openFooterList));
+footerTitleContacts.addEventListener('click', openFooterList)
